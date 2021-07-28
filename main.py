73ebac0b12b7af4c10b9f0df5daea2d08718db94
Jacob <jacob@datacea.com>
2021-07-28 02:21:06 -0400

    more shit is happening. although we might need to consider the sphere avenue..
    this transformation thing doesn't seem to be doing us justice

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import utils.constructors as construct
 from re import findall
 
 
-vertices_by_line, maps_by_line = construct.file_splitter('input/input.obj', 'work/vertex.txt', 'work/map.txt')
-open('output/output.obj', 'w')
+vertices_by_line, maps_by_line = construct.file_splitter('input/gummyRing.obj', 'work/vertex.txt', 'work/map.txt')
+print(f'v: {len(vertices_by_line)}; f: {len(maps_by_line)}')
+open('output/output.obj', 'w').close()
 output_file = open('output/output.obj', 'a')
+
+current_line = 0
 for map in maps_by_line:
     a = {"x": float(), "y": float(), "z": float()}  # reset all points
     b = {"x": float(), "y": float(), "z": float()}  # index 0 is x, index 1 is y, index 2 is z
@@ -24,38 +27,27 @@ for map in maps_by_line:
 
     radius_one = float()
     radius_two = float()
-    point_one = [float(), float(), float()]
-    point_two = [float(), float(), float()]
+    point_one = {'x': float(), 'y': float(), 'z': float()}
+    point_two = {'x': float(), 'y': float(), 'z': float()}
+    point_l = {'x': float(), 'y': float(), 'z': float()}
 
     if longest_side == A:
-        radius_one, radius_two = B, C
-        point_one, point_two = c, b
-        tp_one, tp_two = 0.0, longest_side
-        tp_one_r, tp_two_r = B, C
+        radius_one, radius_two = C, B
+        point_one, point_two, point_l = b, c, a
     elif longest_side == B:
         radius_one, radius_two = A, C
-        point_one, point_two = c, a
-        tp_one, tp_two = 0.0, longest_side
-        tp_one_r, tp_two_r = A, C
+        point_one, point_two, point_l = c, a, b
     else:
         radius_one, radius_two = A, B
-        point_one, point_two = b, a
-        tp_one, tp_two = 0.0, longest_side
-        tp_one_r, tp_two_r = A, B
+        point_one, point_two, point_l = b, a, c
 
 
-    # print(f'For the triangle made by map [{map}]...\nThe longest side has length {longest_side}.\nA: {A}, B: {B}, C: {C}\nPoint 1: {point_one}\nPoint 2: {point_two}\nRadius 1: {radius_one}\nRadius 2: {radius_two}')
-
-    our_geometry = construct.circle_intersection((tp_one, 0.0, tp_one_r),
-                                                 (tp_two, 0.0, tp_two_r))
+    our_geometry = construct.circle_intersection((0.0, 0.0, radius_one),
+                                                 (longest_side, 0.0, radius_two))
 
     (one, two), (three, four) = our_geometry
-    temp_length = ((one + three) / 2)
-    # print(f'Temporary Length: {temp_length}\n')
-
-    # temp_length = 'find length from point_one to the origin.'
+    temp_length = abs((one + three) / 2)
     percent_length = temp_length / longest_side
-    # print(percent_length)
 
     xDifference = abs(point_one['x'] - point_two['x'])
     yDifference = abs(point_one['y'] - point_two['y'])
@@ -83,5 +75,13 @@ for map in maps_by_line:
         origin['z'] = point_one['z']
 
     output_file.write(f"v {origin['x']} {origin['y']} {origin['z']}\n")
-    print(f"v {origin['x']} {origin['y']} {origin['z']}")
+    output_file.write(f"v {point_l['x']} {point_l['y']} {point_l['z']}\n")
+    output_file.write(f"v {point_one['x']} {point_one['y']} {point_one['z']}\n")
+    output_file.write(f"v {point_two['x']} {point_two['y']} {point_two['z']}\n")
+    current_line += 5
 
+    output_file.write(f"f {current_line-4} {current_line-3} {current_line-2}\n")
+    output_file.write(f"f {current_line-4} {current_line-3} {current_line-1}\n")
+    current_line += 1
+
+    print(f'For the triangle made by map [{map}]...\nThe longest side has length {longest_side}.\nA: {A}, B: {B}, C: {C}\nPoint 1: {point_one}\nPoint 2: {point_two}\nRadius 1: {radius_one}\nRadius 2: {radius_two}\n')
