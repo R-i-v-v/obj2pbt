5c9413f7a9f6f0f393f0c914c0eb4a2dc916723b
Rivvnik <jacob@datacea.com>
2021-08-02 13:47:24 -0400

    use dot products to find orthogonal lines

--- main.py
+++ main.py
@@ -35,25 +35,43 @@ with scandir('input') as dirs:
             output_file = open(f'output/{entry.name[:-4]}_origins.obj', 'a')
 
         current_line = 0
-        for triangle_map in face_maps_by_line:
+        for triangle_map in face_maps_by_line:  # iterate through each f-map line
             a, b, c = {}, {}, {}  # reset points to empty dicts
             for target_line, point in zip([int(x.split('/')[0] if '/' in x else x) for x in [s for s in findall(r'-?\d+\.?\d*/?\d*/?\d*', triangle_map)]], [a, b, c]):
                 for value, index in zip([float(x) for x in findall(r'-?\d+\.?\d*', vertices_by_line[target_line-1])], ["x", "y", "z"]):
                     point[index] = value
 
-            # set lines
-            AB = sqrt(((a['x'] - b['x']) ** 2) + ((a['y'] - b['y']) ** 2) + ((a['z'] - b['z']) ** 2))
-            BC = sqrt(((b['x'] - c['x']) ** 2) + ((b['y'] - c['y']) ** 2) + ((b['z'] - c['z']) ** 2))
-            AC = sqrt(((a['x'] - c['x']) ** 2) + ((a['y'] - c['y']) ** 2) + ((a['z'] - c['z']) ** 2))
+            # get vectors AB, AC, and BC
+            ab_v = {'x': a['x'] - b['x'], 'y': a['y'] - b['y'], 'z': a['z'] - b['z']}
+            ac_v = {'x': a['x'] - c['x'], 'y': a['y'] - c['y'], 'z': a['z'] - c['z']}
+            bc_v = {'x': b['x'] - c['x'], 'y': b['y'] - c['y'], 'z': b['z'] - c['z']}
 
+            # calculate lengths of AB, AC, and BC by squaring vectors
+            AB = sqrt((ab_v['x'] ** 2) + (ab_v['y'] ** 2) + (ab_v['z'] ** 2))
+            AC = sqrt((ac_v['x'] ** 2) + (ac_v['y'] ** 2) + (ac_v['z'] ** 2))
+            BC = sqrt((bc_v['x'] ** 2) + (bc_v['y'] ** 2) + (bc_v['z'] ** 2))
+
+            # if right triangle, write vertex corresponding to pi/2 radians to output file
             if max(AB ** 2, BC ** 2, AC ** 2) == (AB ** 2 + BC ** 2 + AC ** 2 - max(AB ** 2, BC ** 2, AC ** 2)):
-                continue
+                if (ab_v['x'] * ac_v['x']) + (ab_v['y'] * ac_v['y']) + (ab_v['z'] * ac_v['z']) == 0:
+                    output_file.write(f"v {a['x']} {a['y']} {a['z']}\n")
+                elif (ab_v['x'] * bc_v['x']) + (ab_v['y'] * bc_v['y']) + (ab_v['z'] * bc_v['z']) == 0:
+                    output_file.write(f"v {b['x']} {b['y']} {b['z']}\n")
+                else:
+                    output_file.write(f"v {c['x']} {c['y']} {c['z']}\n")
+                continue  # move on and start from the top with the next triangle
 
+            # given that the triangle is not a right triangle...
+            # we need to get the longest side, as well as the two points it falls in between
+            # we need to use the two other sides as sphere radii
             longest_side, point_one, point_two, radius_one, radius_two = (AB, b, a, BC, AC) if BC < AB and AC < AB else (BC, c, b, AC, AB) if AC < BC and AB < BC else (AC, c, a, BC, AB)
 
+            # i don't know what h represents, but it's required for the intersection equation below.
             h = 1 / 2 + (radius_one ** 2 - radius_two ** 2) / (2 * longest_side ** 2)
 
             origin = {}
+            # compute the x, y, and z coordinates of the point that lies at the center of
+            # the circle of intersection between two spheres of aforementioned radii
             for coord in ['x', 'y', 'z']:
                 origin[coord] = point_one[coord] + h * (point_two[coord] - point_one[coord])
 
