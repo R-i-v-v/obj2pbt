1b91dcef99ae26e6b1ff90859260c31a0b6a198a
Rivvnik <jacob@datacea.com>
2021-08-02 18:29:00 -0400

    made set_dict concise

--- main.py
+++ main.py
@@ -9,64 +9,56 @@ from json import dumps
 
 def set_dict(o, point, opposite, file):
     output = {
-        'o': {  # origin point
-            'x': o['x'],
-            'y': o['y'],
-            'z': o['z']
-        },
-        'a': {  # one of longest_side's endpoints
-            'x': point['x'],
-            'y': point['y'],
-            'z': point['z']
-        },
-        'b': {  # point opposite the origin
-            'x': opposite['x'],
-            'y': opposite['y'],
-            'z': opposite['z']
-        }
+        'o': o,  # origin point
+        'a': point,  # one of longest_side's endpoints
+        'b': opposite  # point opposite the origin
     }
     file.write(f'{dumps(output)}\n')
 
+# iterate through files in input directory
 with scandir('input') as dirs:
     for entry in dirs:
         start_time, rights_count = time(), 0
+
+        # reset vertex, map, origins, and output if they exist
+        # read input and output files into memory
         open('work/vertex.txt', 'w').close()
         open('work/map.txt', 'w').close()
         open('work/origins.obj', 'w').close()
+        open(f'output/{entry.name[:-4]}', 'w').close()
         vertex_file = open('work/vertex.txt', 'a')
         map_file = open('work/map.txt', 'a')
         obj_file = open('work/origins.obj', 'a')
         input_file = open(f'input/{entry.name}', 'r')
         input_lines = input_file.readlines()
+        output_file = open(f'output/{entry.name[:-4]}', 'a')
+
+        # extract vertices and faces from input .obj file
         for line in input_lines:
             if line.startswith('v '):
                 vertex_file.write(line[2:])
             elif line.startswith('f '):
                 map_file.write(line[2:])
-
         vertex_file.close()
         map_file.close()
 
+        # get vertices by line
         vertices_by_line = [n.strip() for n in open('work/vertex.txt', 'r').readlines()]
         face_maps_by_line = [n.strip() for n in open('work/map.txt', 'r').readlines()]
         print(f'<{entry.name}> {len(vertices_by_line)} vertices and {len(face_maps_by_line)} faces')
 
-        open(f'output/{entry.name[:-4]}', 'w').close()
-        output_file = open(f'output/{entry.name[:-4]}', 'a')
-
-        current_line = 0
         for triangle_map in face_maps_by_line:  # iterate through each f-map line
             a, b, c = {}, {}, {}  # reset points to empty dicts
             for target_line, point in zip([int(x.split('/')[0] if '/' in x else x) for x in [s for s in findall(r'-?\d+\.?\d*/?\d*/?\d*', triangle_map)]], [a, b, c]):
                 for value, index in zip([float(x) for x in findall(r'-?\d+\.?\d*', vertices_by_line[target_line-1])], ["x", "y", "z"]):
                     point[index] = value
 
-            # get vectors AB, AC, and BC.
+            # get vectors AB, AC, and BC
             ab_v = {'x': a['x'] - b['x'], 'y': a['y'] - b['y'], 'z': a['z'] - b['z']}
             ac_v = {'x': a['x'] - c['x'], 'y': a['y'] - c['y'], 'z': a['z'] - c['z']}
             bc_v = {'x': b['x'] - c['x'], 'y': b['y'] - c['y'], 'z': b['z'] - c['z']}
 
-            # calculate lengths AB, AC, and BC by squaring vectors and taking the root of the sum
+            # calculate lengths AB, AC, and BC by squaring vectors and taking the root of their sum
             ab_l = sqrt((ab_v['x'] ** 2) + (ab_v['y'] ** 2) + (ab_v['z'] ** 2))
             ac_l = sqrt((ac_v['x'] ** 2) + (ac_v['y'] ** 2) + (ac_v['z'] ** 2))
             bc_l = sqrt((bc_v['x'] ** 2) + (bc_v['y'] ** 2) + (bc_v['z'] ** 2))
@@ -78,7 +70,7 @@ with scandir('input') as dirs:
                                                                           bc_l ** 2,
                                                                           ac_l ** 2)):
                 rights_count += 1
-                flip = {}
+                flip = {}  # what the hell even ARE dot products, anyway??
                 if (ab_v['x'] * ac_v['x']) + (ab_v['y'] * ac_v['y']) + (ab_v['z'] * ac_v['z']) == 0:
                     flip = a  # if vectors AB and AC are orthogonal, origin is a
                 elif (ab_v['x'] * bc_v['x']) + (ab_v['y'] * bc_v['y']) + (ab_v['z'] * bc_v['z']) == 0:
@@ -86,14 +78,14 @@ with scandir('input') as dirs:
                 else:
                     flip = c  # vectors AC and BC must be orthogonal, thus origin is c
                 obj_file.write(f"v {flip['x']} {flip['y']} {flip['z']}\n")
-                set_dict({'x': flip['x'], 'y': flip['y'], 'z': flip['z']},
+                set_dict(flip,
                          b if flip == a else c if flip == b else a,
                          c if flip == a else a if flip == b else b,
                          output_file)
                 continue  # move on and start from the top with the next triangle
 
-            # we need to get the longest side, as well as the two points it falls in between
-            # we need to use the two other sides as sphere radii
+            # get the longest side, as well as the two points it falls in between
+            # get the point opposite the longest side and use other two sides as sphere radii
             longest_side, point_one, point_two, point_three, radius_one, radius_two = (ab_l, b, a, c, bc_l, ac_l) if bc_l < ab_l and ac_l < ab_l else (bc_l, c, b, a, ac_l, ab_l) if ac_l < bc_l and ab_l < bc_l else (ac_l, c, a, b, bc_l, ab_l)
 
             # i don't know what h represents, but it's required for the intersection equation below.
