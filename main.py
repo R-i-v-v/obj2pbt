74fdbfa498b893a19c6cfd82f33a86ad715ceee4
Jacob <jacob@datacea.com>
2021-08-07 10:48:56 -0400

    ebic changes

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from time import time
 from decimal import Decimal
 from re import findall
 from os import scandir
-from json import dumps, loads
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 from generatePBT import PBT
@@ -58,17 +57,18 @@ def find_right_angle(a, b, c):
     else:
         return None
 
+
 np.set_printoptions(16)
 
 # iterate through files in input directory
 with scandir('input') as dirs:
     for entry in dirs:
         start_time = time()
-        pbt_output = PBT(name='EpicPythonGeneratedTemplate')
+        pbt_output = PBT(name='ebic_python_generated_template')
 
         # reset vertex, map, origins, and output if they exist
         # read input and output files into memory
-        open('work/vertex.txt', 'w').close(), open('work/map.txt', 'w').close(), open('work/triplets', 'w').close()
+        open('work/vertex.txt', 'w').close(), open('work/map.txt', 'w').close()
         open(f'output/{entry.name[:-4]}.pbt', 'w').close()
         vertex_file, map_file = open('work/vertex.txt', 'a'), open('work/map.txt', 'a')
         input_file, output_file = open(f'input/{entry.name}', 'r'), open(f'output/{entry.name[:-4]}.pbt', 'a')
@@ -86,8 +86,8 @@ with scandir('input') as dirs:
         vertices_by_line = [n.strip() for n in open('work/vertex.txt', 'r').readlines()]
         face_maps_by_line = [n.strip() for n in open('work/map.txt', 'r').readlines()]
 
-        for triangle_map in face_maps_by_line:  # iterate through each f-map line
-            a, b, c = [], [], []  # reset points to empty dicts
+        for triangle_map in face_maps_by_line:  # iterate through each face map
+            a, b, c = [], [], []  # reset vectors to empty lists
             for target_line, point in zip([int(x.split('/')[0] if '/' in x else x) for x in [s for s in findall(r'-?\d+\.?\d*/?\d*/?\d*', triangle_map)]], [a, b, c]):
                 for value in [float(x) for x in findall(r'-?\d+\.?\d*', vertices_by_line[target_line-1])]:
                     point.append(value)
@@ -98,6 +98,6 @@ with scandir('input') as dirs:
 
         output_file.write(pbt_output.generate_pbt())
 
-        output_file.close()
+        input_file.close(), output_file.close()
         print(f'<{entry.name}> {len(vertices_by_line)} vertices and {len(face_maps_by_line)} faces')
-        print(f'Found {len(open(f"work/origins.obj", "r").readlines())} origins and {len(open(f"work/triplets", "r").readlines())} triangles.\nFinished in {round(Decimal(time() - start_time) * 1000, 3)} ms.\n')
+        print(f'Finished in {round(Decimal(time() - start_time) * 1000, 3)} ms.\n')
