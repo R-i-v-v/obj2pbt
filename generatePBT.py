924793d50790b33af1a6f295d73c8b4bbe7f0089
Rivvnik <jacob@datacea.com>
2021-08-07 13:29:39 -0400

    cleaned up pbt format issues

--- generatePBT.py
+++ generatePBT.py
@@ -1,7 +1,9 @@
 import random
 
+
 def generate_id():
     return f'{random.randrange(10**19, 10**20)}'
+
 
 class Object:
     def __init__(self, name, position, rotation, scale, parent_id, mesh_id):
@@ -12,6 +14,7 @@ class Object:
         self.parent_id = parent_id
         self.mesh_id = mesh_id
         self.id = generate_id()
+
 
 class PBT:
     def __init__(self, name):
@@ -50,124 +53,128 @@ class PBT:
     def children_to_string(self):
         children_string = ""
         for object in self.objects:
-            children_string += "\n ChildIds: " + object.id
+            children_string += f"ChildIds: {object.id}\n        "
         return children_string
 
     def all_objects_pbt(self):
         all_objects_string = ""
 
         for object in self.objects:
-            object_string = f"""
-                            Objects {{
-                                Id: {object.id}
-                                Name: "{object.name}"
-                                Transform {{
-                                    Location {{
-                                        X: {object.position[0]}
-                                        Y: {object.position[1]}
-                                        Z: {object.position[2]}
-                                    }}
-                                    Rotation {{
-                                        Pitch: {object.rotation[0]}
-                                        Yaw: {object.rotation[1]}
-                                        Roll: {object.rotation[2]}
-                                    }}
-                                    Scale {{
-                                        X: {object.scale[0]}
-                                        Y: {object.scale[1]}
-                                        Z: {object.scale[2]}
-                                    }}
-                                }}
-                                ParentId: {self.root_id}    
-                                Collidable_v2 {{
-                                    Value: "mc:ecollisionsetting:inheritfromparent"
-                                }}
-                                Visible_v2 {{
-                                    Value: "mc:evisibilitysetting:inheritfromparent"
-                                }}
-                                CameraCollidable {{
-                                    Value: "mc:ecollisionsetting:inheritfromparent"
-                                }}
-                                EditorIndicatorVisibility {{
-                                    Value: "mc:eindicatorvisibility:visiblewhenselected"
-                                }}
-                                CoreMesh {{
-                                    MeshAsset {{
-                                        Id: {object.mesh_id}
-                                    }}
-                                    Teams {{
-                                        IsTeamCollisionEnabled: true
-                                        IsEnemyCollisionEnabled: true
-                                    }}
-                                    StaticMesh {{
-                                        Physics {{
-                                            Mass: 100
-                                            LinearDamping: 0.01
-                                        }}
-                                        BoundsScale: 1
-                                    }}
-                                }}
-                            }} \n
-                            """
+            object_string = f"""Objects {{
+        Id: {object.id}
+        Name: "{object.name}"
+        Transform {{
+          Location {{
+            X: {object.position[0]}
+            Y: {object.position[1]}
+            Z: {object.position[2]}
+          }}
+          Rotation {{
+            Pitch: {object.rotation[0]}
+            Yaw: {object.rotation[1]}
+            Roll: {object.rotation[2]}
+          }}
+          Scale {{
+            X: {object.scale[0]}
+            Y: {object.scale[1]}
+            Z: {object.scale[2]}
+          }}
+        }}
+        ParentId: {self.root_id}    
+        Collidable_v2 {{
+          Value: "mc:ecollisionsetting:inheritfromparent"
+        }}
+        Visible_v2 {{
+          Value: "mc:evisibilitysetting:inheritfromparent"
+        }}
+        CameraCollidable {{
+          Value: "mc:ecollisionsetting:inheritfromparent"
+        }}
+        EditorIndicatorVisibility {{
+          Value: "mc:eindicatorvisibility:visiblewhenselected"
+        }}
+        CoreMesh {{
+          MeshAsset {{
+            Id: {object.mesh_id}
+          }}
+          Teams {{
+            IsTeamCollisionEnabled: true
+            IsEnemyCollisionEnabled: true
+          }}
+          StaticMesh {{
+            Physics {{
+              Mass: 100
+              LinearDamping: 0.01
+            }}
+            BoundsScale: 1
+          }}
+        }}
+      }}\n      """
             all_objects_string += object_string
         return all_objects_string
 
     def object_assets_pbt(self):     
         assets_string = ""
         for mesh in self.meshes_by_id:
-            mesh_asset_string = f"""
-                Assets {{
-                    Id: {mesh['id']}
-                    Name: "{mesh['name']}"
-                    PlatformAssetType: 1
-                    PrimaryAsset {{
-                        AssetType: "StaticMeshAssetRef"
-                        AssetId: "{mesh['name']}"
-                    }}
-                }}
-            """
+            mesh_asset_string = f"""Assets {{
+      Id: {mesh['id']}
+      Name: "{mesh['name']}"
+      PlatformAssetType: 1
+      PrimaryAsset {{
+        AssetType: "StaticMeshAssetRef"
+        AssetId: "{mesh['name']}"
+      }}
+    }}"""
             assets_string += mesh_asset_string
         return assets_string
 
     def generate_pbt(self):
-        pbt = f"""
-            Assets {{
-                Id: {self.template_id}
-                Name: "{self.template_name}"
-                PlatformAssetType: 5
-                TemplateAsset {{
-                    ObjectBlock {{
-                        RootId: {self.root_id}
-                        Objects {{
-                            Id: {self.root_id}
-                            Name: "Group"
-                            Transform {{
-                                Location {{
-                                }}
-                                Rotation {{
-                                }}
-                                Scale {{
-                                    X: 1
-                                    Y: 1
-                                    Z: 1
-                                }}
-                            }}
-                            {self.children_to_string()}
-                            Folder {{
-                                IsGroup: true
-                            }}
-                        }}
-                        {self.all_objects_pbt()}
-                    }}
-                    {self.object_assets_pbt()}
-                    PrimaryAssetId {{
-                        AssetType: "None"
-                        AssetId: "None"
-                    }}
-                }}
-                SerializationVersion: 92
-            }}
-        """
+        pbt = f"""Assets {{
+  Id: {self.template_id}
+  Name: "{self.template_name}"
+  PlatformAssetType: 5
+  TemplateAsset {{
+    ObjectBlock {{
+      RootId: {self.root_id}
+      Objects {{
+        Id: {self.root_id}
+        Name: "Group"
+        Transform {{
+          Location {{
+          }}
+          Rotation {{
+          }}
+          Scale {{
+            X: 1
+            Y: 1
+            Z: 1
+          }}
+        }}
+        {self.children_to_string()}Collidable_v2 {{
+          Value: "mc:ecollisionsetting:inheritfromparent"
+        }}
+        Visible_v2 {{
+          Value: "mc:evisibilitysetting:inheritfromparent"
+        }}
+        CameraCollidable {{
+          Value: "mc:ecollisionsetting:inheritfromparent"
+        }}
+        EditorIndicatorVisibility {{
+          Value: "mc:eindicatorvisibility:visiblewhenselected"
+        }}
+        Folder {{
+          IsGroup: true
+        }}
+      }}
+      {self.all_objects_pbt()[:-2]}}}
+    {self.object_assets_pbt()}
+    PrimaryAssetId {{
+      AssetType: "None"
+      AssetId: "None"
+    }}
+  }}
+  SerializationVersion: 92
+}}"""
         return pbt
 
 
