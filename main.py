seemingly fixed texture issues and scaling issues. compiled.

main.py
@@ -60,8 +60,8 @@ app.buttonize()
 def run(path):
     global file_path
     parent = str(Path(path).parent) if not Path(path).is_dir() else str(path)
-    if app.log.get() == 1 and not exists(Path(f'{parent}/log.csv')):
-        open(f'{parent}/log.csv', 'w').write('File name\tFace count\tTime elapsed (ms)\n')
+    # if app.log.get() == 1 and not exists(Path(f'{parent}/log.csv')):
+    #     open(f'{parent}/log.csv', 'w').write('File name\tFace count\tTime elapsed (ms)\n')
     entries = []
     if not file_path:
         with scandir(path) as dirs:
@@ -190,7 +190,7 @@ def run(path):
                 if position is not None and scale is not None and rotation is not None:
                     mesh_index += 1
                     child_name = "{0}.{1:04}".format(group_names[group] if g_count > 0 else 'mesh', mesh_index)
-                    folders[group].add_child(child_name, "sm_wedge_002", np.multiply(position, 10), rotation, np.multiply(scale, 10), None, color, app.texturize.get() == 1)
+                    folders[group].add_child(child_name, "sm_wedge_001", np.multiply(position, 10), rotation, np.multiply(scale, 10), None, color, app.texturize.get() == 1)
                 else:
                     continue
 
@@ -201,8 +201,8 @@ def run(path):
         app.lbl.place_forget()
         app.aesthetic_path.set('')
         file_path = ''
-        if log_file:
-            log_file.write(f'{entry_name}\t{face_count}\t{round(Decimal(time()-start_time)*1000,3)}\n')
+        # if log_file:
+        #     log_file.write(f'{entry_name}\t{face_count}\t{round(Decimal(time()-start_time)*1000,3)}\n')
     app.buttonize()
 
 

utils/calc.py
@@ -39,7 +39,7 @@ def triangle(a, b, c, should_optimize):
 
         # position calculation - our triangles are corner-aligned wedges, so position is where the right angle occurs
         # which is always point c in our program
-        position = c
+        position = np.divide(np.add(a, c), 2)
 
         # scale calculation
         scale = np.divide([0.02, len_ac, len_bc], 100)
@@ -64,10 +64,12 @@ def triangle(a, b, c, should_optimize):
 
     # position calculation - our triangles are corner-aligned wedges, so position is where the right angle occurs
     # which is the same for both triangles, since they share the point where their right angles occur
-    position_1 = position_2 = np.add(a, l1)
+    o = np.add(a, l1)
+    r = np.multiply(x, 0.0002)
+    position_1 = position_2 = np.divide(np.add(c, np.add(o, r)), 2)
 
     # scale calculation
-    scale_1, scale_2 = np.divide([0.02, width, np.linalg.norm(l1)], 100), np.divide([0.02, width, np.linalg.norm(l2)], 100)
+    scale_1, scale_2 = np.divide([0.002, width, np.linalg.norm(l1)], 100), np.divide([0.002, width, np.linalg.norm(l2)], 100)
 
     # Rotation calculation
     matrix_1, matrix_2 = np.transpose([x, -y, -z]), np.transpose([-x, -y, z])